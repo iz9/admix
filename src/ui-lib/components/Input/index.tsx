bdd5ffe3c79633c18c0dbdf4b3c0d37b64d3eda4
iz9 <dev.iz9.l.n@gmail.com>
2021-10-08 02:51:28 +0300

    feat(ui/components): input component

---
+++ src/ui-lib/components/Input/index.tsx
@@ -0,0 +1,57 @@
+/** @jsxImportSource theme-ui */
+
+import React from 'react'
+import type { ThemeUICSSObject } from 'theme-ui'
+import { getInputStyles, getIconContainerStyles, IconPosition } from './styles'
+import { ThemeColor } from '../../theme'
+import { Property } from 'csstype'
+
+type RenderIconFn = () => JSX.Element
+interface InputProps {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value: string
+  /** text and border color */
+  color?: ThemeColor | Property.Color
+  disabled?: boolean
+  placeholder?: string
+  /** returns JSX markdown with <SvgIcon/> element */
+  renderIconAfter?: RenderIconFn
+  /** returns JSX markdown with <SvgIcon/> element */
+  renderIconBefore?: RenderIconFn
+  /** ThemeUI styling object */
+  sx?: ThemeUICSSObject
+}
+
+/**
+ *
+ * Input component
+ *
+ */
+export const Input: React.FC<InputProps> = ({
+  color,
+  disabled,
+  renderIconAfter,
+  renderIconBefore,
+  sx,
+  ...props
+}) => {
+  const styles = getInputStyles({
+    color,
+    isIconBefore: Boolean(renderIconBefore),
+    isIconAfter: Boolean(renderIconAfter),
+  })
+
+  const renderIcon = (position: IconPosition) => {
+    const styles = getIconContainerStyles(position)
+    const fn = position === 'before' ? renderIconBefore : renderIconAfter
+    return fn ? <div sx={styles}>{fn()}</div> : null
+  }
+
+  return (
+    <div sx={{ position: 'relative' }}>
+      {renderIcon('before')}
+      <input {...{ sx: { ...styles, ...sx }, disabled, ...props }} />
+      {renderIcon('after')}
+    </div>
+  )
+}
---
+++ src/ui-lib/components/Input/styles.ts
@@ -0,0 +1,63 @@
+import type { ThemeUICSSObject } from 'theme-ui'
+import { ThemeColor } from '../../theme'
+import { Property } from 'csstype'
+
+export type IconPosition = 'before' | 'after'
+export const getIconContainerStyles = (
+  position: IconPosition,
+): ThemeUICSSObject => {
+  const styles: ThemeUICSSObject = {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: '40px',
+    zIndex: 2,
+  }
+  if (position === 'before') {
+    styles.left = 0
+  } else if (position === 'after') {
+    styles.right = 0
+  }
+  return styles
+}
+
+export const getInputStyles = ({
+  color = 'black300',
+  isIconAfter,
+  isIconBefore,
+}: {
+  color?: ThemeColor | Property.Color
+  isIconBefore?: boolean
+  isIconAfter?: boolean
+}): ThemeUICSSObject => {
+  const styles: ThemeUICSSObject = {
+    borderColor: 'gray700',
+    borderRadius: '10px',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    color,
+    fontSize: 'm',
+    padding: '12px',
+    position: 'relative',
+    width: '100%',
+    '&:focus': {
+      borderColor: 'black300',
+      outline: 'none',
+    },
+    ['&::placeholder']: {
+      color,
+    },
+  }
+
+  if (isIconBefore) {
+    styles.paddingLeft = '40px'
+  }
+  if (isIconAfter) {
+    styles.paddingRight = '40px'
+  }
+
+  return styles
+}
